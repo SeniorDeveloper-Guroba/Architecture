38c7af5035c0633bcade12858f008528642342bd
Aiub Duduev <aub.duduev.1989@gmail.com>
2023-03-29 19:02:32 +0300

    no message

--- Sources/Architecture/ViewProtocol.swift
+++ Sources/Architecture/ViewProtocol.swift
@@ -5,8 +5,6 @@ public protocol ViewProtocol where Self: AnyObject {
     
     associatedtype ViewProperties
     
-    var viewProperties: ViewProperties? { get set }
-    
     func create(with viewProperties: ViewProperties?)
     func update(with viewProperties: ViewProperties?)
 }
